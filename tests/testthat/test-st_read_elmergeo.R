d58ca138f81bf4ba7508824941d3b69e145dfe02
Chris Peak <cpeak@psrc.org>
2022-12-07 17:20:58 -0800

    add some basic tests

---
+++ tests/testthat/test-st_read_elmergeo.R
@@ -0,0 +1,19 @@
+test_that("multiplication works", {
+  expect_equal(2 * 2, 4)
+})
+
+test_that("st_read_elmergeo('county_background') does not return an error", {
+  expect_error(relmer::st_read_elmergeo('county_background'), NA)
+})
+
+test_that("st_read_elmergeo('fakefake') does return an error", {
+  expect_error(relmer::st_read_elmergeo('fakefake'))
+})
+
+
+test_that('errors can occur', {
+  expect_error(1/"a")
+})
+
+  relmer::st_read_elmergeo('fakefake')
+  
