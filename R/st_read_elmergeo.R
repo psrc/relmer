562b156c683b33fbed550b3011e814e1737a80b0
jensenmj <mjensen@psrc.org>
2022-12-08 12:26:53 -0800

    Switch reproject_sf to 2 args.

--- R/st_read_elmergeo.R
+++ R/st_read_elmergeo.R
@@ -1,5 +1,5 @@
 get_conn <- function(dbname='ElmerGeo') {
-  
+
   tryCatch({
     DBI::dbConnect(odbc::odbc(),
               driver = "SQL Server",
@@ -14,14 +14,14 @@ get_conn <- function(dbname='ElmerGeo') {
 }
 
 build_sql <- function(schema_name, tbl_name, conn) {
-  
+
   tryCatch({
     col_name_ns_sql <- glue::glue("select c.COLUMN_NAME
             from INFORMATION_SCHEMA.COLUMNS c
             where c.TABLE_SCHEMA = '{schema_name}'
             	and c.TABLE_NAME = '{tbl_name}'
             	and c.DATA_TYPE not in ('geometry', 'geography')
-            	and c.COLUMN_NAME not in ('GDB_GEOMATTR_DATA')") 
+            	and c.COLUMN_NAME not in ('GDB_GEOMATTR_DATA')")
     col_df = DBI::dbGetQuery(conn, DBI::SQL(col_name_ns_sql))
     col_names_ns <- paste(t(col_df), collapse=', ')
     s_col_name_sql <- glue::glue("select c.COLUMN_NAME + '.STAsBinary() as Shape '
@@ -29,7 +29,7 @@ build_sql <- function(schema_name, tbl_name, conn) {
             where c.TABLE_SCHEMA = '{schema_name}'
             	and c.TABLE_NAME = '{tbl_name}'
             	and c.DATA_TYPE in ('geometry', 'geography')
-            	and c.COLUMN_NAME not in ('GDB_GEOMATTR_DATA')") 
+            	and c.COLUMN_NAME not in ('GDB_GEOMATTR_DATA')")
     s_col_df = DBI::dbGetQuery(conn, DBI::SQL(s_col_name_sql))
     col_names_s <- paste(t(s_col_df), collapse=', ')
     glue::glue('SELECT {col_names_ns}, {col_names_s} FROM {schema_name}.{tbl_name}')
@@ -41,7 +41,7 @@ build_sql <- function(schema_name, tbl_name, conn) {
 }
 
 layer_type <- function(layer_name, schema_name, conn) {
-  
+
   tryCatch({
     if (is_table(layer_name, schema_name, conn, as_evw=TRUE)) {
       table_type <- 'evw'
@@ -59,16 +59,16 @@ layer_type <- function(layer_name, schema_name, conn) {
 }
 
 is_table <- function(layer_name, schema_name, conn, as_evw = FALSE){
-  
+
   tryCatch({
     suffix <- ifelse(as_evw, '_evw', '')
-    info_schema_sql <- glue::glue("SELECT TABLE_NAME 
+    info_schema_sql <- glue::glue("SELECT TABLE_NAME
                             FROM INFORMATION_SCHEMA.VIEWS as v
                             WHERE v.TABLE_NAME = \'{layer_name}{suffix}\'
                               AND v.TABLE_SCHEMA = \'{schema_name}\'")
     info_schema_df <- DBI::dbGetQuery(conn, DBI::SQL(info_schema_sql))
     if (nrow(info_schema_df) > 0) {
-      TRUE 
+      TRUE
     } else {
       FALSE
     }
@@ -80,15 +80,15 @@ is_table <- function(layer_name, schema_name, conn, as_evw = FALSE){
 }
 
 is_evw <- function(layer_name, schema_name, conn){
-  
+
   tryCatch({
-    info_schema_sql <- glue::glue("SELECT TABLE_NAME 
+    info_schema_sql <- glue::glue("SELECT TABLE_NAME
                             FROM INFORMATION_SCHEMA.VIEWS as v
                             WHERE v.TABLE_NAME = \'{layer_name}_evw\'
                               AND v.TABLE_SCHEMA = \'{schema_name}\'")
     info_schema_df <- DBI::dbGetQuery(conn, DBI::SQL(info_schema_sql))
     if (nrow(info_schema_df) > 0) {
-      TRUE 
+      TRUE
     } else {
       FALSE
     }
@@ -99,10 +99,10 @@ is_evw <- function(layer_name, schema_name, conn){
   })
 }
 
-reproject_sf <- function(lyr, in_epsg, out_epsg) {
-  
+reproject_sf <- function(lyr, out_epsg) {
+
   tryCatch({
-    sf::st_crs(lyr, in_epsg)
+    in_epsg <- sf::st_crs(lyr)
     if (in_epsg != out_epsg) {
       sf::st_transform(lyr, out_epsg)
     }
@@ -117,28 +117,27 @@ reproject_sf <- function(lyr, in_epsg, out_epsg) {
 
 #' st_read_elmergeo(layer_name, ...)
 #'
-#' read simple features from PSRC's in-house geodatabase #' 
+#' read simple features from PSRC's in-house geodatabase #'
 #' This function is preferable to sf::st_read() when pulling
 #' from Elmer or ElmerGeo, because it avoids some incompatibilities
 #' between SQL Server's data types and R's.
-#' 
+#'
 #' @param layer_name The name of the feature layer or geodatabase table
 #' @param schema_name The name of the schema that layer_name exists in.  Defaults to "dbo", the standard schema for ElmerGeo tables
 #' @param project_to_wgs84 (TRUE/FALSE) If TRUE then deliver the output in WGS84 projection, otherwise NAD84 / WA State Plane North.  Defaults to TRUE.
 #' @return object of class sf
 #'
 #' @note If the layer has been set up as an ESRI versioned layer in the geodatabase, this function returns the versioned view (which exists in SQL Server with a "_evw" suffix).  If it has not been set up that way, it returns the base table.
-#' 
-#' @examples 
+#'
+#' @examples
 #' st_read_elmergeo("COUNTY_BACKGROUND")
 #'
 #' st_read_elmergeo("COUNTY_BACKGROUND", project_to_wgs84 = FALSE)
 #'
 #' @export
 st_read_elmergeo <- function(layer_name, schema_name='dbo', project_to_wgs84 = TRUE) {
-  
+
   tryCatch({
-    elmergeo_srid <- 2285
     conn <- get_conn()
     if (layer_type(layer_name, schema_name, conn) == 'evw') {
       tbl_name <- glue::glue("{layer_name}_evw")
@@ -149,9 +148,10 @@ st_read_elmergeo <- function(layer_name, schema_name='dbo', project_to_wgs84 = T
       stop()
     }
     layer_sql <- build_sql(schema_name=schema_name, tbl_name=tbl_name, conn)
-    lyr <- sf::st_read(conn, query=layer_sql)
-    srid <- ifelse(project_to_wgs84, 4326, elmergeo_srid)
-    reproject_sf(lyr, elmergeo_srid, srid)
+    lyr <- sf::st_read(conn, query=layer_sql) %>% sf::st_crs(2285)
+    if(project_to_wgs84){
+      reproject_sf(lyr, 4326)
+    }
     return(lyr)
   }, warning = function(w) {
     print(glue::glue("A warning popped up in st_read_elmergeo: {w}"))
