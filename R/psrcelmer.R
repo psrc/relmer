ad9503c8775895f006282b74b0dbb3717704f9ee
Chris Peak <cpeak@psrc.org>
2022-12-20 14:30:35 -0800

    add roxygen file for the package.

---
+++ R/psrcelmer.R
@@ -0,0 +1,12 @@
+#' psrcelmer: A package for easy retrieval of data from PSRC's databases Elmer and ElmerGeo
+#'
+#'
+#' @author Chris Peak \email{cpeak@psrc.org}
+#' @author Mike Jensen \email{mjensen@psrc.org}
+#'
+#' \bold{Homepage}: \url{https://psrc.github.io/psrcelmer/}
+#'
+#' @docType package
+#' @name psrcelmer
+#'
+"_PACKAGE"
